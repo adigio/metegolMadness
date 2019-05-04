adae05d578d55e50536d2ce11f354d1929160eea
Alan Di Gio <adigio@outlook.com>
2019-05-04 18:01:17 -0300

    Updates

---
+++ precache-manifest.6b366d5ef35094f1d60855fa95d853dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21f5dad9e7d870e23274",
+    "url": "/metegolMadness/static/css/main.05d7f8c7.chunk.css"
+  },
+  {
+    "revision": "21f5dad9e7d870e23274",
+    "url": "/metegolMadness/static/js/main.21f5dad9.chunk.js"
+  },
+  {
+    "revision": "317bc4f418e5a950413e",
+    "url": "/metegolMadness/static/js/runtime~main.317bc4f4.js"
+  },
+  {
+    "revision": "85141d1d8fa7091e9753",
+    "url": "/metegolMadness/static/js/2.85141d1d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/metegolMadness/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d12a958292bfa07a4b80ed365e851e97",
+    "url": "/metegolMadness/index.html"
+  }
+];
